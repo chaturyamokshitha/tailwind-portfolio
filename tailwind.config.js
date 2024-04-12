0f506d90da0344c613d22723d29f502969a5fb38
chaturyamokshitha <chaturyamokshitha@gmail.com>
2024-04-12 20:10:36 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        back: "#1A1C28;",
+      },
+      
+    },
+  },
+  plugins: [],
+};
